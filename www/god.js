ebb02a03b9446de87db02ff087cf729efe31b0d8
saltybot <saltybot@gmail.com>
2014-07-04 11:38:58 -0500

    Center image and window; remove padding/margin

---
+++ www/god.js
@@ -0,0 +1,9 @@
+function god() {
+  // get config
+  // start god machine
+}
+
+function resizeAndCenterWindow(width, height) {
+  window.resizeTo(width, height);
+  window.moveTo(((screen.width - width) / 2), ((screen.height - height) / 2));
+}
